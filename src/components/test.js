c357692b15ceb5fcbb2628675677791299d2d0b0
Muthu Kumar S <45813425+muthukumar3@users.noreply.github.com>
2022-09-20 17:11:52 +0530

    New learning concepts

--- src/components/test.js
+++ src/components/test.js
@@ -1,9 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 
 export default function Test() {
+
+  useEffect(() => {
+
+    return () => {
+      // clear state here
+    }
+  }, []);
+
+  const multiLineString = `Hi 
+  this is Multiline string
+  3 lines here`;
+
     return(
       <Fragment>
-        Hii
+        <li> Let Const </li>
+        <li> Multi Line String </li>
+        {multiLineString}
+        <li> Default Parameters </li>
+        <li> Template Literals </li>
+        <li> Array or Object Destructuring  </li>
       </Fragment>
     );
 }
