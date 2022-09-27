78e3f8cb9b0011098e73da013f2c441f5a523a75
Muthu Kumar S <45813425+muthukumar3@users.noreply.github.com>
2022-09-27 21:59:43 +0530

    New changes

--- src/components/test.js
+++ src/components/test.js
@@ -1,26 +1,10 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { memo } from 'react';
 
-export default function Test() {
-
-  useEffect(() => {
-
-    return () => {
-      // clear state here
-    }
-  }, []);
-
-  const multiLineString = `Hi 
-  this is Multiline string
-  3 lines here`;
-
+const MemoTest = () =>  {
     return(
-      <Fragment>
-        <li> Let Const </li>
-        <li> Multi Line String </li>
-        {multiLineString}
-        <li> Default Parameters </li>
-        <li> Template Literals </li>
-        <li> Array or Object Destructuring  </li>
-      </Fragment>
+        <h5> Hi </h5>
     );
 }
+
+export default memo(MemoTest);
+
